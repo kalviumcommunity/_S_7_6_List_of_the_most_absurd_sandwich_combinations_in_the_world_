e356e6d6aa60ade84c9be3aeb493835437315366
harsheni10160 <devaharsheni2006@gmail.com>
2025-02-13 08:57:40 +0530

    changes made and pushed

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ const mongoose =require("mongoose");
 require('dotenv').config();
 
 const connectDatabase =() =>{
-    mongoose.connect(process.env.db_url)
+    mongoose.connect(process.env.db_URL)
     .then((data) =>{
         console.log(`MongoDB connected with server: ${data.connection.host}`);
     })
